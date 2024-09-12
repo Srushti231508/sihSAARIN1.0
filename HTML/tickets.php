07eb3465d943a2cf7b9178537208685fa3de4ffc
Srushtisodavadiya <sodavadiyasrushti@gmail.com>
2024-09-12 18:13:22 +0530

    Update tickets.php

--- HTML/tickets.php
+++ HTML/tickets.php
@@ -127,7 +127,7 @@ $password = ""; // your DB password
 $dbname = "museum_ticketing_system";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli1($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
