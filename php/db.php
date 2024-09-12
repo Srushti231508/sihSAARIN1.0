6cddcf57887408f745da37871a9a1923669359c5
Het <het.vaghasiya2004@gmail.com>
2024-09-12 14:54:41 +0530

    php

--- php/db.php
+++ php/db.php
@@ -1,30 +1,15 @@
 <?php
-header('Content-Type: application/json');
-
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "root"; // Default WAMP MySQL user
+$password = ""; // Leave empty if no password is set
 $dbname = "museum_ticketing_system";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT * FROM your_table_name";
-$result = $conn->query($sql);
-
-$data = array();
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-} else {
-    echo json_encode([]);
-}
-
-echo json_encode($data);
-
-$conn->close();
+echo "Connected successfully";
 ?>
